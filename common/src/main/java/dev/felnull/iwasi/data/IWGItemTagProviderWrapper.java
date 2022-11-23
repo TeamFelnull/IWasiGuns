64e16b431670b45f9b94ca00ad4c377b51f82071
MORIMORI0317 <morimori.0317@outlook.jp>
2022-11-23 21:44:33 +0900

    nna

--- common/src/main/java/dev/felnull/iwasi/data/IWGItemTagProviderWrapper.java
+++ common/src/main/java/dev/felnull/iwasi/data/IWGItemTagProviderWrapper.java
@@ -4,6 +4,7 @@ import dev.felnull.iwasi.item.IWGItemTags;
 import dev.felnull.otyacraftengine.data.CrossDataGeneratorAccess;
 import dev.felnull.otyacraftengine.data.provider.BlockTagProviderWrapper;
 import dev.felnull.otyacraftengine.data.provider.ItemTagProviderWrapper;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,7 +16,7 @@ public class IWGItemTagProviderWrapper extends ItemTagProviderWrapper {
     @Override
     public void generateTag(ItemTagProviderAccess providerAccess) {
         providerAccess.tag(IWGItemTags.MEAT_FOODS).add(Items.PORKCHOP, Items.COOKED_PORKCHOP, Items.BEEF, Items.COOKED_BEEF, Items.CHICKEN, Items.COOKED_CHICKEN, Items.ROTTEN_FLESH, Items.RABBIT, Items.COOKED_RABBIT, Items.MUTTON, Items.COOKED_MUTTON);
-        providerAccess.tag(IWGItemTags.FISH_FOODS).add(Items.COD, Items.PUFFERFISH, Items.COOKED_COD, Items.SALMON, Items.COOKED_SALMON);//.addTag(ItemTags.FISHES);
+        //providerAccess.tag(IWGItemTags.FISH_FOODS).addVanillaTag(ItemTags.FISHES);
         providerAccess.tag(IWGItemTags.GRAIN_FOODS).add(Items.BREAD);
         providerAccess.tag(IWGItemTags.VEGETABLE_FOODS).add(Items.CARROT, Items.POTATO, Items.POISONOUS_POTATO, Items.BEETROOT);
         providerAccess.tag(IWGItemTags.SEAWEED_FOODS).add(Items.KELP);
